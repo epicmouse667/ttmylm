c490ff096e0e377eb3f9ac0c24a7b91f75010407
unknown <epicmouse>
2022-05-31 20:26:10 +0800

    update commentlist.go in controller layer

--- controller/commentList.go
+++ controller/commentList.go
@@ -1,1 +1,37 @@
 package controller
+
+import (
+	"dou_sheng/pogo"
+	"dou_sheng/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CommentListResponse struct {
+	Response    Response
+	CommentList []pogo.Comment
+}
+
+func CommentList(c *gin.Context) {
+	token := c.Query("token")
+	video_id, _ := strconv.Atoi(c.Query("video_id"))
+	commentlist, flag := service.CommentList(token, video_id)
+	if flag {
+		c.JSON(http.StatusOK, CommentListResponse{
+			Response: Response{
+				StatusCode: 0,
+				StatusMsg:  "Success",
+			}, CommentList: *commentlist,
+		})
+	} else {
+		c.JSON(http.StatusOK,
+			CommentListResponse{
+				Response: Response{
+					StatusCode: 1,
+					StatusMsg:  "Failed to fetch comment list.",
+				}})
+
+	}
+}
